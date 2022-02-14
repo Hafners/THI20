04587e603be07431f04fab828a3ed12054a3a372
Nina Erkelenz <voldehorn@web.de>
2022-02-14 11:09:27 +0100

    Konsole Grundfunktionen

--- Kundenverwaltung/Program.cs
+++ Kundenverwaltung/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Konsolenausgabe
+Console.WriteLine("Hier könnte Ihre Werbung stehen!");
+
+// Konsoleneingabe
+string? userInput = Console.ReadLine();
+
+// Konsole Textfarbe verändern
+Console.ForegroundColor = ConsoleColor.Red;
+
+// Konsole Hintergrundfarbe verändern
+Console.BackgroundColor = ConsoleColor.White;
+
+// Konsole warten auf eine Eingabe
+Console.ReadKey();
