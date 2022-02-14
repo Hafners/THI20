7258291e770477a1703c1246b5b50d5190809f5e
Markus Heimbach <markus.heimbach@it.bit-gendorf.de>
2022-02-14 10:38:23 +0100

    test

--- Data/kunde_heimbach.cs
+++ Data/kunde_heimbach.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace Data
 {
-	public Class1()
-	{
-	}
+    class kunde_heimbach
+    {
+    }
 }
