608f1c97ed97e4aed1e79f6fb0d85ea2c102c64d
Stefan Hafner <stefan.hafner.development@gmail.com>
2022-02-14 11:08:55 +0100

    added console class

---
+++ Kundenverwaltung/A_Console.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kundenverwaltung
+{
+    internal class A_Console
+    {
+        public void Example()
+        {
+            // Konsolenausgabe
+            Console.WriteLine("Hier könnte Ihre Werbung stehen!");
+
+            // Konsoleneingabe
+            string? userInput = Console.ReadLine();
+
+            // Konsole Textfarbe verändern
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            // Konsole Hintergrundfarbe verändern
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            // Konsole warten auf irgendeine Eingabe
+            Console.ReadKey();
+        }
+    }
+}
--- Kundenverwaltung/Program.cs
+++ Kundenverwaltung/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿// Konsolenausgabe
+Console.WriteLine("Hier könnte Ihre Werbung stehen!");
+
+// Konsoleneingabe
+string? userInput = Console.ReadLine();
+
+// Konsole Textfarbe verändern
+Console.ForegroundColor = ConsoleColor.Red;
+
+// Konsole Hintergrundfarbe verändern
+Console.BackgroundColor = ConsoleColor.Red;
+
+// Konsole warten auf irgendeine Eingabe
+Console.ReadKey();
