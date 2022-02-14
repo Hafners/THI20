bdf6aac713056e9350168438758383391c4b2b46
Luca Rose <luca.rose@it.bit-gendorf.de>
2022-02-14 10:49:03 +0100

    added new class Kunde_Rose.cs

--- Data/Kunde_Rose.cs
+++ Data/Kunde_Rose.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Kunde_Rose
+namespace Data
 {
-	public Kunde_Rose()
-	{
-	}
+    internal class Kunde_Rose
+    {
+    }
 }
