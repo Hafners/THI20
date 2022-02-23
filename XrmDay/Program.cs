602436e1fc5b380fe35acbe8ccd1e7b04e304eba
Dom <Dominik.gepper@live.de>
2022-02-23 10:09:49 +0100

    Dom

--- XrmDay/Program.cs
+++ XrmDay/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using XrmDay.MainCall;
+using XrmDay.Data;
 
 namespace XrmDay
 {
@@ -8,6 +9,12 @@ namespace XrmDay
         static void Main(string[] args)
         {
             new main_Kain().execute(); 
+            //TEST VON GEPPER
+            Customer_Gepper kunde_herrmann = new Customer_Gepper();
+            kunde_herrmann.saveUserData();
+            kunde_herrmann.ausgeben();
+            //ENDE VON GEPPER
+
         }
     }
 }
