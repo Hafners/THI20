562c7bf3ca80a107182f5570764c9f076c22605a
Nina Erkelenz <voldehorn@web.de>
2022-02-23 18:40:42 +0100

    more xml try

--- XrmDay/Program.cs
+++ XrmDay/Program.cs
@@ -3,6 +3,7 @@ using System;
 using XrmDay.Data;
 using System.Xml;
 using XrmDay.Data.Erkelenz;
+using System.Collections.Generic;
 
 namespace XrmDay
 {
@@ -13,16 +14,30 @@ class Program
     static void Main(string[] args)
     {
         //new Main_Erkelenz().Execute();
-     
+
         /* Logic: 
          *  - check if file exists
          *      if yes -> append
          *      else -> create
          */
 
+        List<Customer_Erkelenz> list = new List<Customer_Erkelenz> { 
+            new Customer_Erkelenz(),
+
+        }
         Customer_Erkelenz customer = new Customer_Erkelenz();
         customer.Name = "Nina";
         customer.Id = 1;
+
+        var customerlist = utility.GetCustomerList();
+        var xmlfromLINQ = new XElement("customers",
+                    from c in customerlist
+                    select new XElement("customer",
+                        new XElement("CustomerId", c.CustomerId),
+                        new XElement("CustomerName", c.CustomerName),
+                        new XElement("PhoneNumber", c.PhoneNumber),
+                        new XElement("Email", c.Email)
+                        ));
 
         XmlTextWriter textWriter = new XmlTextWriter("..\\..\\..\\Data\\Erkelenz\\Files\\data.xml", null);
         textWriter.WriteStartDocument();
