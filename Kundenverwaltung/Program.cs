acd452ae5dd7f7e5c4c9963cdc9a891489e1a04e
Markus Kerscher <markuskensv@gmail.com>
2022-02-23 09:02:25 +0100

    test

--- Kundenverwaltung/Program.cs
+++ Kundenverwaltung/Program.cs
@@ -3,3 +3,28 @@
 Console.WriteLine("Bitte gib eine Id ein");
 string userInputId = Console.ReadLine();
 
+class Customer
+{
+    private
+    int ID;
+    int customernumber;
+    string name;
+    string zipcode; 
+    enum Country
+    {
+        Germany;
+        England; 
+        Austria; 
+        Brasil; 
+    }
+    string street;
+    string city;
+    public void Methode()
+    {
+        Console.WriteLine("Bitte eine ID eingeben");
+
+        string? userInput = Console.ReadLine();
+    }
+
+
+}
