d43087cbd5d69d3ee24f1f5a652c6feeca876f2e
Stefan Hafner <stefan.hafner.development@gmail.com>
2022-03-21 11:13:44 +0100

    added something

---
+++ XrmRestApi/Data/Hafner_Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace XrmRestApi.Data
+{
+    public class Hafner_Customer
+    {
+        #region Properties
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CustomerNumber { get; set; }
+
+        public string Zipcode { get; set; }
+
+        #endregion
+    }
+}
--- XrmRestApi/Program.cs
+++ XrmRestApi/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using XrmRestApi;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(connectionString));
+
 
 // Add services to the container.
 
